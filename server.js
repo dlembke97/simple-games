81a67e418fcac7d9e6a01b4d4ce2fe8e79cb0cf3
dlembke3 <dlembke3@gatech.edu>
2024-08-10 16:03:32 -0500

    working tic tac toe

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 4000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve the static files from the frontend/build directory
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+// Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
